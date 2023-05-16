dc43d94c082d14798849c7f3d53c8e86c104f82e
98ultimate <mikko@notsofool.com>
2023-05-16 14:37:43 +0300

    remove extra slash

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment'
 
 export const title = 'NotSoFool'
 export const description = 'SvelteKit testing'
-export const url = dev ? 'http://localhost:5173/' : 'https://blog.notsofool.com/'
+export const url = dev ? 'http://localhost:5173' : 'https://blog.notsofool.com'
